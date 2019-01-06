f887b174e644c56048edbfb3950e4fdc37ada70a
Riddhi Sharma <rshar59@uwo.ca>
2019-01-06 19:57:49 +0000

    Updated the Routes

--- myrestroapp/src/app/app-routing.module.ts
+++ myrestroapp/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+ 
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
